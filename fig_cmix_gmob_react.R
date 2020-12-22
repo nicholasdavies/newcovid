89df1ad3bcd2f957a4a9c4b2d91f0e9928349be5
Tim Russell <timothywilliamrussell@gmail.com>
2020-12-22 15:00:08 +0000

    adding Sebs subplot and tweaking whole plot

--- fig_cmix_gmob_react.R
+++ fig_cmix_gmob_react.R
@@ -4,6 +4,9 @@
 library(data.table)
 library(ggplot2)
 library(patchwork)
+library(dplyr)
+library(tidyr)
+library(lubridate)
 
 # Source user written scripts ---------------------------------------------
 source('get_react_data.R')
@@ -12,8 +15,6 @@ theme_set(cowplot::theme_cowplot(font_size = 8) + theme(strip.background = eleme
 
 cols_2 = c("#d575f0", "#6deda5")
 cols_3 = c("#d575f0", "#6deda5", "#000000")
-
-
 
 # Mappings ----------------------------------------------------------------
 
@@ -80,56 +81,116 @@ plot.dt[,
       )))
 ]
 
-p_var <- ggplot(plot.dt[newvariant == TRUE & area != "ZAF"]) + aes(date) +
-    annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-02"), ymin=-Inf, ymax=Inf, col = "lightgrey", alpha = 0.2) +
-    geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
-    geom_vline(aes(xintercept = as.Date("2020-12-02")), linetype = 2) +
-    facet_grid() +
-    geom_ribbon(aes(ymin = lo95, ymax = hi95, fill =area), alpha = 0.1) +
-    geom_ribbon(aes(ymin = lo50, ymax = hi50, fill=area), alpha = 0.2) +
-    geom_line(aes(y=N/total, col = area)) +
-    scale_x_date(
-        name = NULL,
-        date_breaks = "months", date_minor_breaks = "weeks",
-        date_labels = "%b"
-    ) +
-    scale_color_manual(values = cols_3, name = "") +
-    scale_fill_manual(values = cols_3, name = "") +
-    scale_y_continuous("Novel Variant Fraction", expand = expansion(0)) +
-    coord_cartesian(ylim = c(0, 1), xlim = c(as.Date("2020-10-01"), NA)) +
-    theme(
-    )
-p_var
+#--- OLD VERSION
+# p_var <- ggplot(plot.dt[newvariant == TRUE & area != "ZAF"]) + aes(date) +
+#     annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-02"), ymin=-Inf, ymax=Inf, col = "lightgrey", alpha = 0.2) +
+#     geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
+#     geom_vline(aes(xintercept = as.Date("2020-12-02")), linetype = 2) +
+#     facet_grid() +
+#     geom_ribbon(aes(ymin = lo95, ymax = hi95, fill =area), alpha = 0.1) +
+#     geom_ribbon(aes(ymin = lo50, ymax = hi50, fill=area), alpha = 0.2) +
+#     geom_line(aes(y=N/total, col = area)) +
+#     scale_x_date(
+#         name = NULL,
+#         date_breaks = "months", date_minor_breaks = "weeks",
+#         date_labels = "%b"
+#     ) +
+#     scale_color_manual(values = cols_3, name = "") +
+#     scale_fill_manual(values = cols_3, name = "") +
+#     scale_y_continuous("Novel Variant Fraction", expand = expansion(0)) +
+#     coord_cartesian(ylim = c(0, 1), xlim = c(as.Date("2020-10-01"), NA)) +
+#     theme(
+#     )
 
 
-
-# Rt corr plot ------------------------------------------------------------
-
-p_corr <- ggplot(plot.dt[newvariant == TRUE & area == "ZAF"]) + aes(date) +
-  annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-02"), ymin=-Inf, ymax=Inf, col = "lightgrey", alpha = 0.2) +
-  geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
-  geom_vline(aes(xintercept = as.Date("2020-12-02")), linetype = 2) +
-  facet_grid() +
-  geom_ribbon(aes(ymin = lo95, ymax = hi95, fill =area), alpha = 0.1) +
-  geom_ribbon(aes(ymin = lo50, ymax = hi50, fill=area), alpha = 0.2) +
+#--- Tims version
+p_var <- plot.dt[newvariant == TRUE & area != "ZAF" & date <= "2020-12-01"] %>%
+  ggplot(aes(x = date)) +
   geom_line(aes(y=N/total, col = area)) +
+  geom_ribbon(aes(ymin = lo95, ymax = hi95, fill = area), alpha = 0.1) +
+  geom_ribbon(aes(ymin = lo50, ymax = hi50, fill = area), alpha = 0.2) +
+  annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-01"), 
+           ymin = -Inf, ymax = Inf, col = "lightgrey", alpha = 0.2) +
+  geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
+  facet_grid() +
   scale_x_date(
     name = NULL,
     date_breaks = "months", date_minor_breaks = "weeks",
     date_labels = "%b"
   ) +
-  scale_color_manual(values = "#000000", name = "") +
-  scale_fill_manual(values = "#000000", name = "") +
-  scale_y_continuous("Novel Variant Fraction", expand = expansion(0)) +
-  coord_cartesian(ylim = c(0, 1), xlim = c(as.Date("2020-10-01"), NA)) +
-  theme(
-    legend.position = "none"
-  )
+  scale_color_manual(values = cols_3, name = "", labels = c("East of England,\nLondon and South\nEast regions",
+                                                            "Rest of England")) +
+  scale_fill_manual(values = cols_3, name = "", labels = c("East of England,\nLondon and South\nEast regions",
+                                                           "Rest of England")) +
+  scale_y_continuous("Novel variant proportion", expand = expansion(0)) +
+  coord_cartesian(xlim = c(as.Date("2020-10-01"), as.Date("2020-12-01"))) + 
+  theme(legend.position = c(0.01, 0.98)) + 
+  labs(title = "A")
+
+p_var
+
+# Rt corr plot ------------------------------------------------------------
+
+week_start <- 5
+
+english_pillars <- readRDS("data/english_pillars.rds") %>%
+  filter(between(date_specimen,
+                 as.Date("2020-09-01"), as.Date("2020-12-10"))) %>%
+  mutate(week_specimen = floor_date(date_specimen, "week",
+                                    week_start = week_start))
+
+rt_estimates <-
+  paste0("https://raw.githubusercontent.com/epiforecasts/covid-rt-estimates/",
+         "master/subnational/united-kingdom-local/cases/summary/rt.csv")
+
+suppressMessages(rt <- vroom::vroom(rt_estimates))
+
+by_ltla <- english_pillars %>%
+  select(-lower_age_limit, -positive, -total) %>%
+  group_by(pillar, week_specimen, nhser_name, ltla_name, ltla_code) %>%
+  summarise_if(is.numeric, sum) %>%
+  ungroup() %>%
+  pivot_longer(starts_with("sgene"), names_to = "sgene_result",
+               values_to = "n") %>%
+  mutate(sgene_result = sub("^sgene_", "", sgene_result))
+
+
+rt_by_ltla <- rt %>%
+  rename(ltla_name = region) %>%
+  filter(type == "estimate")
+
+
+rt_weekly <- rt_by_ltla %>%
+  mutate(week_infection = floor_date(date, "week", week_start = week_start)) %>%
+  group_by(ltla_name, week_infection) %>%
+  summarise(mean = mean(mean), sd = mean(sd), n = n(), .groups = "drop") %>%
+  filter(n == 7) %>%
+  select(-n)
+
+by_ltla_rt <- by_ltla %>%
+  filter(pillar == "Pillar 2") %>%
+  mutate(week_infection = week_specimen - 7) %>%
+  select(-pillar, -negative) %>%
+  pivot_wider(names_from = sgene_result, values_from = n) %>%
+  mutate(prop_variant = negative / (positive + negative),
+         cases = n_a + negative + positive) %>%
+  inner_join(rt_weekly, by = c("week_infection", "ltla_name")) %>%
+  select(week_infection, nhser_name, ltla_name, ltla_code, prop_variant, cases,
+         rt_mean = mean, rt_sd = sd)
+
+p_corr <- by_ltla_rt %>% 
+  filter(week_infection == max(week_infection)) %>%
+  #mutate(cases = cases*0.01) %>%
+  ggplot(aes(x = prop_variant, y = rt_mean, fill = nhser_name, size = cases), scale_size = 0.25) +
+  geom_jitter(pch = 21) +
+  scale_fill_brewer("", palette = "Set1") +
+  xlab("Proportion with S gene dropped") +
+  ylab("Mean reproduction number") +
+  labs(title = "B", 
+       size = "Cases since 9 October")
+
+
 p_corr
-
-
-
-
 
 # Mobility data -----------------------------------------------------------
 gmob <- qs::qread('data/gm_for_analysis.qs')
@@ -154,22 +215,32 @@ gmob_tier[, variable := factor(variable, levels = c(
                                "Transit stations",
                                "Parks"))]
 
-p_gmob <- ggplot(gmob_tier[date > as.Date("2020-09-01")], aes(x = date)) +
-    annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-02"), ymin=-Inf, ymax=Inf, col = "lightgrey", alpha = 0.2) +
-    geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
-    geom_vline(aes(xintercept = as.Date("2020-12-02")), linetype = 2) +
-    geom_smooth(aes(y = value, col = area, fill = area)) +
-    facet_wrap(variable ~. , scales = "free_y", ncol = 1) +
-    scale_x_date(breaks = "2 week", date_labels = "%d-%b", expand = expansion(0)) +
-    scale_color_manual(values = cols_2, name = "") +
-    scale_fill_manual(values = cols_2, name = "") +
-    expand_limits(y = 0) +
-    labs(title = "A", y = "% change in mobility", x = "") +
-    geom_hline(aes(yintercept = 0), linetype = 2) +
-    theme(
-        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-        legend.key = element_rect(size = 6, fill = "white", colour = NA), legend.key.size = unit(1, "cm")
-    )
+gmob_tier[, setattr(area, "levels", c("South East\nEast of England,\nand London\nTier 4 areas*", "Rest of England"))]
+
+p_gmob <- gmob_tier[date > as.Date("2020-09-01")] %>% 
+  ggplot(aes(x = date)) +
+  geom_smooth(aes(y = value, col = area, fill = area)) +
+  geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
+  geom_vline(aes(xintercept = as.Date("2020-12-02")), linetype = 2) +
+  annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-02"), ymin=-Inf, ymax=Inf, col = "lightgrey", alpha = 0.2) +
+  facet_wrap(variable ~. , scales = "free_y", ncol = 1) +
+  scale_x_date(breaks = "2 week", date_labels = "%d-%b", expand = expansion(0)) +
+  scale_color_manual(values = cols_3, name = "", labels = c("East of England,\nLondon and South\nEast regions",
+                                                            "Rest of England")) +
+  scale_fill_manual(values = cols_3, name = "", labels = c("East of England,\nLondon and South\nEast regions",
+                                                           "Rest of England")) +
+  expand_limits(y = 0) +
+  labs(title = "C", y = "Relative change in mobility (%)", x = "") +
+  geom_hline(aes(yintercept = 0), linetype = 2) +
+  theme(
+    legend.position = "none",
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+    legend.key = element_rect(size = 6, fill = "white", colour = NA), legend.key.size = unit(1, "cm")
+  ) + 
+  labs(title = "C")
+  
+
+p_gmob
 # Average contacts over time ----------------------------------------------
 
 
@@ -177,39 +248,35 @@ avg_contacts <- qs::qread("data/cmix_avg_cnts.qs")
 
 avg_contacts[, area := factor(area,  levels = c("Tier 4", "Not Tier 4"), label = c("Entry to Tier 4", "Outside of Tier 4"))]
 
-p_cmix <-  
-    ggplot(avg_contacts[!age %in% c("All", "Adult")], aes(x = start_date)) +
-    annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-02"), ymin=-Inf, ymax=Inf, col = "lightgrey", alpha = 0.2) +
-    geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
-    geom_vline(aes(xintercept = as.Date("2020-12-02")), linetype = 2) +
-    expand_limits(y = 0) +
-    geom_ribbon(aes(ymin = lci, ymax = uci, fill =area), alpha = 0.3) +
-    geom_line( aes(y = med, col = area)) +
-    #geom_smooth( aes(y = med), method = "gam") +
-    #geom_point( aes(y = avg)) +
-    labs(title = "B", y = "Mean contacts", x = "") +
-    facet_grid(setting ~ age , scales = "free_y") +
-    scale_y_continuous(expand = expansion(0)) +
-    scale_fill_manual(values = cols_2, name = "") +
-    scale_color_manual(values = cols_2, name = "") +
-    theme(text = element_text(size = 8)) +
-    scale_x_date(breaks = "2 week", date_labels = "%d-%b") +
-    expand_limits(x = as.Date("2020-10-01")) + 
-    theme(
-        legend.position = "none",
-        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-        panel.spacing.y =  unit(1, "lines"), 
-        
-    )
+p_cmix <- avg_contacts[!age %in% c("All", "Adult")] %>%
+  ggplot(aes(x = start_date)) +
+  annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-02"), ymin=-Inf, ymax=Inf, col = "lightgrey", alpha = 0.2) +
+  geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
+  geom_vline(aes(xintercept = as.Date("2020-12-02")), linetype = 2) +
+  expand_limits(y = 0) +
+  geom_ribbon(aes(ymin = lci, ymax = uci, fill =area), alpha = 0.3) +
+  geom_line( aes(y = med, col = area)) +
+  #geom_smooth( aes(y = med), method = "gam") +
+  #geom_point( aes(y = avg)) +
+  labs(title = "B", y = "Mean contacts", x = "") +
+  facet_grid(setting ~ age , scales = "free_y") +
+  scale_y_continuous(expand = expansion(0)) +
+  scale_color_manual(values = cols_3, name = "", labels = c("East of England,\nLondon and South\nEast regions",
+                                                            "Rest of England")) +
+  scale_fill_manual(values = cols_3, name = "", labels = c("East of England,\nLondon and South\nEast regions",
+                                                           "Rest of England")) +
+  theme(text = element_text(size = 8)) +
+  scale_x_date(breaks = "2 week", date_labels = "%d-%b") +
+  expand_limits(x = as.Date("2020-10-01")) + 
+  theme(
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+    panel.spacing.y =  unit(1, "lines")) + 
+  labs(title = "D")
 
 
 # Input data ----------------------------------------------------------------
 
 cmix_eng <- qs::qread("data/cmix_r0.qs")
-
-
-
-
 
 # Compare to REACT --------------------------------------------------------
 names(cmix_eng)
@@ -222,7 +289,7 @@ p_r0 <- ggplot(cmix_eng) +
     geom_point(aes(x = start_date, y = R_50), fill = "red", col = "grey", pch =21) +
     scale_fill_manual(values = c("#3b70bf", "#7dba5d"), labels = c("Single round", "Two rounds")) +
     scale_y_continuous(expand = expansion(0), limits = c(0,3)) +
-    labs(fill = "REACT R estimate", y = "R", x = "", title = "C")  +
+    labs(fill = "REACT R estimate", y = "R", x = "", title = "E")  +
     scale_x_date(breaks = "month", date_labels = "%b") +
     theme(
         axis.text = element_text(size = 9),
@@ -234,15 +301,24 @@ p_r0 <- ggplot(cmix_eng) +
 
 layout <- "
     AABB
+    AABB
+    AABB
+    CCDD
+    CCDD
+    CCDD
+    CCDD
+    CCDD
     CCDD
     CCDD
     EEEE
+    EEEE
     "
+
 plot_final <- p_var + p_corr + p_gmob + p_cmix + p_r0 +  plot_layout(design = layout)
-
-
-
 
 plot_final
 
-#ggsave(filename = "outputs/Figure_1.png", plot_abc, width = 20, height = 12)
+ggsave(filename = "figure_1_draft.pdf", 
+       plot_final,
+       width = 8,
+       height = 8)
