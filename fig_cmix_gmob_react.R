f97f6413130f0967f0a147bf268a687cdf0aa55d
C Jarvis <Christopher1.jarvis@gmail.com>
2020-12-22 11:52:11 +0000

    Combine Chris and Carl's Figure

--- fig_cmix_gmob_react.R
+++ fig_cmix_gmob_react.R
@@ -10,7 +10,8 @@ source('get_react_data.R')
 
 theme_set(cowplot::theme_cowplot(font_size = 8) + theme(strip.background = element_blank()))
 
-cols = c("#8EC3F3", "#7EA59E")
+cols_2 = c("#d575f0", "#6deda5")
+cols_3 = c("#d575f0", "#6deda5", "#000000")
 
 
 
@@ -25,8 +26,78 @@ map_mobility <- c(
     "workplaces" = "Workplaces"  
 )
 
+
+
+# Spread in ZAF and England -----------------------------------------------
+
+.debug <- "data"
+.args <- if (interactive()) sprintf(c(
+    "%s/nextstrain_groups_ngs-sa_COVID19-ZA-2020.12.17_metadata.tsv",
+    "%s/newlin.csv",
+    "comparison.png"
+), .debug) else commandArgs(trailingOnly = TRUE)
+
+rsa.dt <- fread(.args[1])[Country == "South Africa"][order(`Collection Data`)]
+rsa.dt[, area := "ZAF"]
+uk.dt <- fread(.args[2])[order(sample_date)]
+names(uk.dt)
+
+# Remove rest of UK and split England by region
+uk.dt <- uk.dt[!nhs_name %in% c("Northern Ireland", "Scotland", "Wales")]
+uk.dt[nhs_name%in% c("East of England", "London", "South East"), area := "EOE, Lon, SE"]
+uk.dt[!nhs_name %in% c("East of England", "London", "South East"), area := "Rest of England"]
+
+uk.dt[, table(country, nhs_name)]
+plot.dt <- rbind(
+    rsa.dt[,
+           .(.N, iso3c = "ZAF"),
+           by = .(date=`Collection Data`, newvariant = `Clade`=="20C", area)
+    ],
+    uk.dt[,
+          .(.N, iso3c="GBR"),
+          by=.(date=sample_date, newvariant = var2, area)
+    ]
+)
+
+plot.dt[, total := sum(N), by=.(date, iso3c, area) ]
+
+plot.dt[,
+        c("lo95","hi95") := 
+            as.data.table(t(mapply(
+                function(x, n, p=x/n) binom.test(x, n, p, conf.level = .95)$conf.int,
+                x = N, n = total
+            )))
+][,
+  c("lo50","hi50") := 
+      as.data.table(t(mapply(
+          function(x, n, p=x/n) binom.test(x, n, p, conf.level = .50)$conf.int,
+          x = N, n = total
+      )))
+]
+
+p_var <- ggplot(plot.dt[newvariant == TRUE]) + aes(date) +
+    facet_grid(. ~ area) +
+    geom_ribbon(aes(ymin = lo95, ymax = hi95, fill =area), alpha = 0.1) +
+    geom_ribbon(aes(ymin = lo50, ymax = hi50, fill=area), alpha = 0.2) +
+    geom_line(aes(y=N/total, col = area)) +
+    scale_x_date(
+        name = NULL,
+        date_breaks = "months", date_minor_breaks = "weeks",
+        date_labels = "%b"
+    ) +
+    scale_color_manual(values = cols_3, name = "") +
+    scale_fill_manual(values = cols_3, name = "") +
+    scale_y_continuous("Novel Variant Fraction", expand = expansion(0)) +
+    coord_cartesian(ylim = c(0, 1), xlim = c(as.Date("2020-10-01"), NA))
+p_var
+
+
+
+
+
+
+
 # Mobility data -----------------------------------------------------------
-table(gmob$variable)
 gmob <- qs::qread('data/gm_for_analysis.qs')
 tier_data <- read.csv('data/england_ltla_covid_tiers_2020_12_20.csv')
 tier_data <-  as.data.table(tier_data)
@@ -49,15 +120,15 @@ gmob_tier[, variable := factor(variable, levels = c(
                                "Transit stations",
                                "Parks"))]
 
-plot_a <- ggplot(gmob_tier[date > as.Date("2020-09-01")], aes(x = date)) +
+p_gmob <- ggplot(gmob_tier[date > as.Date("2020-09-01")], aes(x = date)) +
     annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-02"), ymin=-Inf, ymax=Inf, col = "lightgrey", alpha = 0.2) +
     geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
     geom_vline(aes(xintercept = as.Date("2020-12-02")), linetype = 2) +
     geom_smooth(aes(y = value, col = area, fill = area)) +
     facet_wrap(variable ~. , scales = "free_y", ncol = 1) +
     scale_x_date(breaks = "2 week", date_labels = "%d-%b", expand = expansion(0)) +
-    scale_color_manual(values = cols, name = "") +
-    scale_fill_manual(values = cols, name = "") +
+    scale_color_manual(values = cols_2, name = "") +
+    scale_fill_manual(values = cols_2, name = "") +
     expand_limits(y = 0) +
     labs(title = "A", y = "% change in mobility", x = "") +
     geom_hline(aes(yintercept = 0), linetype = 2) +
@@ -72,7 +143,7 @@ avg_contacts <- qs::qread("data/cmix_avg_cnts.qs")
 
 avg_contacts[, area := factor(area,  levels = c("Tier 4", "Not Tier 4"), label = c("Entry to Tier 4", "Outside of Tier 4"))]
 
-plot_b <-  
+p_cmix <-  
     ggplot(avg_contacts[!age %in% c("All", "Adult")], aes(x = start_date)) +
     annotate(geom = "rect", xmin = as.Date("2020-11-04"), xmax = as.Date("2020-12-02"), ymin=-Inf, ymax=Inf, col = "lightgrey", alpha = 0.2) +
     geom_vline(aes(xintercept = as.Date("2020-11-04")), linetype = 2) +
@@ -85,8 +156,8 @@ plot_b <-
     labs(title = "B", y = "Mean contacts", x = "") +
     facet_grid(setting ~ age , scales = "free_y") +
     scale_y_continuous(expand = expansion(0)) +
-    scale_fill_manual(values = cols, name = "") +
-    scale_color_manual(values = cols, name = "") +
+    scale_fill_manual(values = cols_2, name = "") +
+    scale_color_manual(values = cols_2, name = "") +
     theme(text = element_text(size = 8)) +
     scale_x_date(breaks = "2 week", date_labels = "%d-%b") +
     expand_limits(x = as.Date("2020-10-01")) + 
@@ -108,29 +179,33 @@ cmix_eng <- qs::qread("data/cmix_r0.qs")
 
 # Compare to REACT --------------------------------------------------------
 names(cmix_eng)
-plot_c <- ggplot(cmix_eng) +
-    geom_rect(data = react_Rt, aes(ymin = `0.05`, ymax = `0.95`, xmin = time_from, xmax = creation_date, fill = estimate), alpha = 0.9, col = "black") +
-    geom_ribbon(aes(ymin = R_05, ymax = R_95, x = start_date), alpha = 0.2, color='DarkGrey') +
+names(cmix_eng)
+p_r0 <- ggplot(cmix_eng) +
+    geom_rect(data = react_Rt, aes(ymin = `0.05`, ymax = `0.95`, xmin = time_from, xmax = creation_date, fill = estimate), alpha = 0.6) +
+    geom_ribbon(aes(ymin = R_05, ymax = R_95, x = start_date), alpha = 0.2) +
     geom_ribbon(aes(ymin = R_25, ymax = R_75, x = start_date), alpha = 0.3) +
     geom_hline(aes(yintercept = 1), color='black', linetype=2) +
-    geom_point(aes(x = start_date, y = R_50), fill = "red", col = "black", pch =21) +
-    scale_fill_manual(values = c("#8EC3F3", "#7EA59E"), labels = c("Single round", "Two rounds")) +
+    geom_point(aes(x = start_date, y = R_50), fill = "red", col = "grey", pch =21) +
+    scale_fill_manual(values = c("#3b70bf", "#7dba5d"), labels = c("Single round", "Two rounds")) +
     scale_y_continuous(expand = expansion(0), limits = c(0,3)) +
     labs(fill = "REACT R estimate", y = "R", x = "", title = "C")  +
     scale_x_date(breaks = "month", date_labels = "%b") +
-    theme(legend.position = c(0.8, 0.9),
-          axis.text = element_text(size = 10),
-          axis.title.y = element_text(size = 10),
-          strip.text = element_text(size = 12),
-          legend.text = element_text(size = 10),
-          legend.title = element_text(size = 10)
+    theme(
+        axis.text = element_text(size = 9),
+        axis.title.y = element_text(size = 10),
+        strip.text = element_text(size = 12),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 10)
     )
 
 
-plot_ab <- (plot_a | plot_b) + plot_layout(guides = "collect")
+p_mix <- (p_gmob | p_cmix) + plot_layout(guides = "collect")
 
-plot_abc <- plot_ab / plot_c
-plot_abc
+plot_all <- (p_var / p_mix / p_r0) + plot_layout(heights = c(1,2,1))
+plot_all
 
 
+
+p2 <- p / plot_abc
+p2
 #ggsave(filename = "outputs/Figure_1.png", plot_abc, width = 20, height = 12)
